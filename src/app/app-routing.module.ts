1fa06d3495e251fac77dbeba36a9824f146adf51
madhu <bamadhu1995@gmail.com>
2022-05-24 17:03:53 +0530

    2nd api fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BooksComponent } from './books/books.component';
+import { MediaComponent } from './media/media.component';
 
-const routes: Routes = [];
+
+
+
+
+const routes: Routes = [
+
+{path:'media',component:MediaComponent},
+{ path:'books', component:BooksComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
