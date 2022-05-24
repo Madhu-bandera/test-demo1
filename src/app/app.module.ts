010d9ff11a137e1fafaffd05bd222a365d11bae1
madhu <bamadhu1995@gmail.com>
2022-05-24 17:06:50 +0530

    add modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { BooksComponent } from './books/books.component';
+import { MediaComponent } from './media/media.component';
+
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksComponent,
+    MediaComponent,
+  
+
+
  ],
   imports: [
     BrowserModule,
